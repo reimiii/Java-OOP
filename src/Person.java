238b5853c99d8a9bfc366c9821d8126a7618b990
z02 <miigo@duck.com>
2023-02-25 19:28:19 +0700

    Constructor

--- src/Person.java
+++ src/Person.java
@@ -3,6 +3,11 @@ public class Person {
     String address;
     final String country = "ID";
 
+    Person(String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
     void sayHello(String paramName) {
         System.out.println("Hello " + paramName + ", my name is " + name);
     }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,19 +1,11 @@
 public class PersonApp {
     public static void main(String[] args) {
-        var person1 = new Person();
-        person1.name = "Hilmi";
-        person1.address = "Bogor";
-//        person1.country = "ID";
+        var person1 = new Person("Hilmi", "Bogor");
 
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.country);
 
         person1.sayHello("Arieff");
-
-
-        Person person2 = new Person();
-        Person person3;
-        person3 = new Person();
     }
 }
