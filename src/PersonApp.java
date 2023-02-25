2dc674cdc37e5dcf47f0d3a0904fbb90053875d5
z02 <miigo@duck.com>
2023-02-25 19:16:28 +0700

    Field

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,13 +1,17 @@
 public class PersonApp {
     public static void main(String[] args) {
         var person1 = new Person();
-        Person person2 = new Person();
+        person1.name = "Hilmi";
+        person1.address = "Bogor";
+//        person1.country = "ID";
 
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
+
+        Person person2 = new Person();
         Person person3;
         person3 = new Person();
-
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
     }
 }
