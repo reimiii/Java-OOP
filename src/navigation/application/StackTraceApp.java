7c7d3eccd4c8dbaf3b9cbb20c05cd9c1b6e2c14a
z02 <miigo@duck.com>
2023-03-01 02:20:12 +0700

    StackTraceElement v2

--- src/navigation/application/StackTraceApp.java
+++ src/navigation/application/StackTraceApp.java
@@ -3,20 +3,16 @@ package navigation.application;
 public class StackTraceApp {
     public static void main(String[] args) {
         try {
-            String[] names = {
-                    "Hilmi", "Akbar", "Muharrom"
-            };
-            System.out.println(names[20]);
-        } catch (Throwable throwable) {
-            StackTraceElement[] stackTraceElements = throwable.getStackTrace();
-            throwable.printStackTrace();
+            sampleError();
+        }catch (RuntimeException exception){
+            exception.printStackTrace();
         }
     }
 
     public static void sampleError() {
         try {
             String[] names = {
-                "Hilmi", "Akbar", "Muharrom"
+                    "Hilmi", "Akbar", "Muharrom"
             };
 
             System.out.println(names[100]);
